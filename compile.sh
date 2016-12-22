af06f275ffc5417de6338291e06a75c839e599c4
Francesco Palmisano <francesco.palmisano@gmail.com>
2016-12-22 03:31:04 +0100

    configured with I2C on AD7091R5 modified SPI driver to be compatible with SPIDEV

--- compile.sh
+++ compile.sh
@@ -7,9 +7,8 @@ do
 		export ARCH=arm
 	elif [ "$param" = "clean" ]; then
 		make clean
-		make .config
+		make imx6q_marta_defconfig
 	elif [ "$param" = "build" ]; then
-		make
 		make -j4 zImage imx6q-marta.dtb
 		cp arch/arm/boot/zImage ./install_dir/
 		cp arch/arm/boot/dts/imx6q-marta.dtb ./install_dir/
