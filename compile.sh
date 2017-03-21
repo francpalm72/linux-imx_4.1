7188a659d2ab52b0cced406bcd04538866d700f2
Francesco Palmisano <francesco.palmisano@gmail.com>
2017-03-21 18:31:37 +0100

    first review for marta

--- compile.sh
+++ compile.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-source /opt/poky/1.7.1/environment-setup-cortexa9hf-vfp-neon-poky-linux-gnueabi
+source /opt/poky/2.2.1/environment-setup-cortexa9hf-neon-poky-linux-gnueabi
 export LDFLAGS=""
 make 
 export LOADADDR=0x10008000
@@ -8,3 +8,4 @@ mkdir -p install_dir
 cp arch/arm/boot/uImage install_dir
 cp arch/arm/boot/dts/imx6q-marta.dtb install_dir
 make modules_install INSTALL_MOD_PATH=./install_dir/
+
